670a835b2a22644360c83d4403ee6b6821d9df48
Ray Jeong <devops3199@gmail.com>
2022-05-19 20:16:00 +0900

    chore: form prac

---
+++ vue-form/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <form v-on:submit="handleSubmit">
+    <div>
+      <label for="username">Username: </label>
+      <!-- v-model = Two Way Data Binding -->
+      <input type="text" for="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">Password: </label>
+      <input type="password" for="password" v-model="password" />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit: function (e) {
+      e.preventDefault();
+      console.log(this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style></style>
