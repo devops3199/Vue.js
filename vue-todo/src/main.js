bbf08389d889432ee8ecad61e34d92bdd50ea6d2
Ray Jeong <devops3199@gmail.com>
2022-05-25 10:55:49 +0900

    feat: vuex

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPlus, faTrashCan, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { store } from "./vuex/store";
 
 library.add([faPlus, faTrashCan, faCheck]);
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
