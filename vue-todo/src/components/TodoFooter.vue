3a8cd004ecb5fb1c108c761cc42f772d9ecb4b70
Ray Jeong <devops3199@gmail.com>
2022-05-25 22:30:08 +0900

    refactor: mapMutations Footer

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -5,11 +5,13 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   methods: {
-    clearTodo() {
-      this.$store.commit("clearTodoItems");
-    },
+    ...mapMutations({
+      clearTodo: "clearTodoItems",
+    }),
   },
 };
 </script>
