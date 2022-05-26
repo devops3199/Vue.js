c5af39836c630ff793327867a7d287eabd6abf5c
Ray Jeong <devops3199@gmail.com>
2022-05-26 21:31:32 +0900

    chore: more views

--- hacker-news/src/components/TopNavigation.vue
+++ hacker-news/src/components/TopNavigation.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <nav>
-      <router-link to="/news">News</router-link>
-      <router-link to="/jobs">Jobs</router-link>
+      <router-link to="/news">News</router-link> |
+      <router-link to="/jobs">Jobs</router-link> |
       <router-link to="/ask">Ask</router-link>
     </nav>
   </header>
@@ -12,4 +12,20 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+nav {
+  background-color: #42b883;
+  display: flex;
+  color: white;
+  padding: 8px;
+}
+
+a {
+  color: white;
+  padding: 0 8px;
+}
+
+.router-link-exact-active {
+  color: #354953;
+}
+</style>
