6800c2c014fb10812051aa281815a9b6c9b3cb5d
Ray Jeong <devops3199@gmail.com>
2022-05-27 20:13:18 +0900

    style: item

--- hacker-news/src/views/ItemView.vue
+++ hacker-news/src/views/ItemView.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <h3>{{ fetchedItem.title }}</h3>
-    <p>{{ fetchedItem.content }}</p>
+    <section class="author">
+      <small>
+        {{ fetchedItem.time_ago }} by
+        <router-link v-bind:to="`/user/${fetchedItem.user}`">{{
+          fetchedItem.user
+        }}</router-link>
+      </small>
+    </section>
+    <section>
+      <div v-html="fetchedItem.content"></div>
+    </section>
+    <hr />
+    <h4>Comments ({{ fetchedItem.comments_count }})</h4>
+    <section>
+      <div
+        class="comment"
+        v-for="item in fetchedItem.comments"
+        v-bind:key="item"
+      >
+        <div v-html="item.content"></div>
+        <small>{{ item.time_ago }} by {{ item.user }}</small>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -21,4 +43,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.author {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.comment {
+  border: 1px solid #42b883;
+  padding: 8px;
+  margin-bottom: 8px;
+}
+
+.comment > small {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
