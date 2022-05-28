01946d7b559ed0fb5e2e2487a3a328a2a0d8be0b
Ray Jeong <devops3199@gmail.com>
2022-05-28 20:12:47 +0900

    refactor: user profile using slot

--- hacker-news/src/views/UserView.vue
+++ hacker-news/src/views/UserView.vue
@@ -1,23 +1,37 @@
 <template>
-  <div>
-    <p>name: {{ fetchedUserInfo.id }}</p>
-    <p>karma: {{ fetchedUserInfo.karma }}</p>
-    <p>created: {{ fetchedUserInfo.created }}</p>
-  </div>
+  <UserProfile>
+    <h4 slot="username">
+      {{ username }}
+    </h4>
+    <div slot="detail">
+      <p>karma: {{ fetchedUserInfo.karma }}</p>
+      <p>created: {{ fetchedUserInfo.created }}</p>
+    </div>
+  </UserProfile>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import UserProfile from "../components/UserProfile.vue";
 
 export default {
+  data() {
+    return {
+      username: "",
+    };
+  },
   computed: {
     ...mapGetters({
       fetchedUserInfo: "fetchedUserInfo",
     }),
   },
   created() {
-    const { id } = this.$route.params;
-    this.$store.dispatch("FETCH_USER_INFO", id);
+    const { id: username } = this.$route.params;
+    this.$store.dispatch("FETCH_USER_INFO", username);
+    this.username = username;
+  },
+  components: {
+    UserProfile,
   },
 };
 </script>
